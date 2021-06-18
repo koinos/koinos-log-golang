fdbc550064f1e2eba0150acc4a0ed50fd635eb24
theoretical <theoreticalbts@users.noreply.github.com>
2021-06-18 19:29:58 -0400

    Add Debugm / Infom / Warnm / Errorm functions to output consistently formatted log messages

--- log.go
+++ log.go
@@ -1,6 +1,11 @@
 package log
 
-import "go.uber.org/zap"
+import (
+	"errors"
+	"fmt"
+
+	"go.uber.org/zap"
+)
 
 // Debugf logs to the debug level with a format string and associated parameters
 func Debugf(template string, args ...interface{}) {
@@ -40,4 +45,44 @@ func Errorf(template string, args ...interface{}) {
 // Error logs to the error level
 func Error(msg string) {
 	zap.L().Error(msg)
+}
+
+func FormatError(msg string, vars ...interface{}) string {
+	if (len(vars) & 1) != 0 {
+		Errorm("Message did not have even number of arguments", "msg", msg)
+	}
+	result := msg
+	for i := 0; i < len(vars); i += 2 {
+		name, ok := vars[i].(string)
+		if !ok {
+			Errorm("Message did not specify name as string", "msg", msg)
+			break
+		}
+		result += fmt.Sprintf(" - %s is %v", name, vars[i+1])
+	}
+	return result
+}
+
+// Debugm logs to the debug level with consistent logging
+func Debugm(msg string, args ...interface{}) {
+	Debug(FormatError(msg, args))
+}
+
+// Infom logs to the info level with consistent logging
+func Infom(msg string, args ...interface{}) {
+	Info(FormatError(msg, args))
+}
+
+// Warnm logs to the warn level with consistent logging
+func Warnm(msg string, args ...interface{}) {
+	Warn(FormatError(msg, args))
+}
+
+// Errorm logs to the error level with consistent logging
+func Errorm(msg string, args ...interface{}) {
+	Error(FormatError(msg, args))
+}
+
+func NewErrorm(msg string, args ...interface{}) error {
+	return errors.New(FormatError(msg, args))
 }
